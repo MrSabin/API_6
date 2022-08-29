ecd863e04f7a0d7f6887d19594b786964b6b61ae
MrSabin <sabin.21011986@gmail.com>
2022-08-29 21:41:09 +0300

    Got image upload address

--- main.py
+++ main.py
@@ -2,17 +2,39 @@ import requests
 from environs import Env
 
 
-env = Env()
-env.read_env()
-vk_access_token = env.str("VK_ACCESS_TOKEN")
-method = "groups.get"
-api_version = "5.131"
-payload = {
-        "access_token": vk_access_token,
-        "v": api_version
-        }
-url=f"https://api.vk.com/method/{method}"
+def get_groups_info(token):
+    method = "groups.get"
+    api_version = "5.131"
+    payload = {
+            "access_token": token,
+            "v": api_version
+            }
+    url = f"https://api.vk.com/method/{method}"
+    response = requests.get(url, params=payload)
+    response.raise_for_status()
+    print(response.json())
 
-response = requests.get(url, params=payload)
-response.raise_for_status()
-print(response.json())
+
+def get_upload_address(token):
+    method = "photos.getWallUploadServer"
+    api_version = "5.131"
+    payload = {
+            "access_token": token,
+            "v": api_version
+            }
+    url = f"https://api.vk.com/method/{method}"
+    response = requests.get(url, params=payload)
+    response.raise_for_status()
+    print(response.json())
+
+
+def main():
+    env = Env()
+    env.read_env()
+    vk_access_token = env.str("VK_ACCESS_TOKEN")
+    get_upload_address(vk_access_token)
+
+
+if __name__ == "__main__":
+    main()
+
