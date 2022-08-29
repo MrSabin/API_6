b465fbf53353137af9511d5b8b6ee4fb24bbd63a
MrSabin <sabin.21011986@gmail.com>
2022-08-29 22:16:38 +0300

    Fixed get_upload_address()

--- main.py
+++ main.py
@@ -15,24 +15,26 @@ def get_groups_info(token):
     print(response.json())
 
 
-def get_upload_address(token):
+def get_upload_address(token, group_id):
     method = "photos.getWallUploadServer"
     api_version = "5.131"
     payload = {
+            "group_id": group_id,
             "access_token": token,
             "v": api_version
             }
     url = f"https://api.vk.com/method/{method}"
     response = requests.get(url, params=payload)
     response.raise_for_status()
-    print(response.json())
+    return response.json()['response'].get('upload_url')
 
 
 def main():
     env = Env()
     env.read_env()
+    group_id = "215609822"
     vk_access_token = env.str("VK_ACCESS_TOKEN")
-    get_upload_address(vk_access_token)
+    upload_url = get_upload_address(vk_access_token, group_id)
 
 
 if __name__ == "__main__":
