be3699383c6c39153ddc951eca756a1b5bcbf2ec
MrSabin <sabin.21011986@gmail.com>
2022-12-11 18:04:35 +0300

    Blacked download.py

--- download.py
+++ download.py
@@ -7,7 +7,7 @@ import requests
 def get_total_images():
     response = requests.get("https://xkcd.com/info.0.json")
     response.raise_for_status()
-    total_images = response.json()['num']
+    total_images = response.json()["num"]
     return total_images
 
 
@@ -16,8 +16,8 @@ def download_comics(comics_number):
     response = requests.get(xkcd_link)
     response.raise_for_status()
     xkcd_answer = response.json()
-    image_link = xkcd_answer['img']
-    author_comment = xkcd_answer['alt']
+    image_link = xkcd_answer["img"]
+    author_comment = xkcd_answer["alt"]
     parsed_url = urllib.parse.urlsplit(image_link)
     filename = os.path.basename(parsed_url.path)
     response = requests.get(image_link)
